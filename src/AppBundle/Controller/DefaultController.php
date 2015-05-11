20acff64ccdf82f71c031b0fc6e58549e8448b81
Manuel Kiessling <manuel@kiessling.net>
2015-05-11 22:12:23 +0200

    Switched to configurative routing

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,14 +2,10 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/app/example", name="homepage")
-     */
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
