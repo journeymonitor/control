8cd9363a598acdac993b4fb93bb7a423a78333e0
Ingmar Heinrich <ingmar.heinrich@crosslend.com>
2015-05-13 00:50:36 +0200

    Added basic implementation of FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new FOS\UserBundle\FOSUserBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
