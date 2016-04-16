cecec0ad06d8728598d5451562533fb992573639
Manuel Kiessling <manuel@kiessling.net>
2016-04-16 21:49:14 +0200

    Added repo method for efficiently getting testresults and statistics.

--- src/AppBundle/Repository/TestresultRepository.php
+++ src/AppBundle/Repository/TestresultRepository.php
@@ -2,8 +2,32 @@
 
 namespace AppBundle\Repository;
 
+use AppBundle\Entity\Statistics;
+use AppBundle\Entity\Testcase;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
 
 class TestresultRepository extends EntityRepository
 {
+    /**
+     * @param Testcase $testcase
+     * @param int $limit
+     * @return \Doctrine\ORM\Internal\Hydration\IterableResult An IterableResult that hydrates arrays
+     */
+    public function getLatestByTestcaseWithStatisticsIterator(Testcase $testcase, $limit)
+    {
+        $qb = $this->createQueryBuilder('t');
+
+        $q = $qb
+            ->select('t, s')
+            ->leftJoin(Statistics::class, 's')
+            ->where($qb->expr()->eq('s.testresult', 't.id'))
+            ->andWhere($qb->expr()->eq('t.testcase', ':testcase'))
+            ->getQuery()
+            ->setParameter(':testcase', $testcase)
+            ->setMaxResults($limit);
+
+        return $q->iterate(null, Query::HYDRATE_ARRAY);
+    }
+
 }
---
+++ tests/AppBundle/Repository/TestresultRepositoryTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\AppBundle\Repository;
+
+use AppBundle\Entity\Statistics;
+use AppBundle\Entity\Testcase;
+use AppBundle\Entity\Testresult;
+use AppBundle\Repository\TestresultRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Tests\AppBundle\TestHelpers;
+
+class TestresultRepositoryTest extends WebTestCase
+{
+    use TestHelpers;
+
+    public function testGetLatestByTestcaseWithStatisticsIterator()
+    {
+        $this->resetDatabase();
+        $client = $this->createAndActivateDemoUser();
+
+        $kernel = $client->getKernel();
+        $kernel->boot();
+
+        $container = $kernel->getContainer();
+
+        /** @var \Doctrine\ORM\EntityManager $em */
+        $em = $container->get('doctrine')->getManager();
+
+        $testcaseRepo = $em->getRepository('AppBundle\Entity\Testcase');
+        /** @var Testcase $testcase */
+        $testcase = $testcaseRepo->findOneBy(['title' => 'Demo User Testcase One']);
+
+        /** @var Testresult $testresult */
+        $testresult = new Testresult();
+        $testresult->setId('abc');
+        $testresult->setExitCode(0);
+        $testresult->setDatetimeRun(new \DateTime('now', new \DateTimeZone('Europe/Berlin')));
+        $testresult->setOutput('');
+        $testresult->setTestcase($testcase);
+        $em->persist($testresult);
+        $em->flush();
+
+        /** @var Statistics $statistics */
+        $statistics = new Statistics();
+        $statistics->setTestresult($testresult);
+        $statistics->setRuntimeMilliseconds(1000);
+        $statistics->setNumberOf200(2);
+        $statistics->setNumberOf400(4);
+        $statistics->setNumberOf500(5);
+        $em->persist($statistics);
+        $em->flush();
+
+        /** @var TestresultRepository $testresultRepo */
+        $testresultRepo = $em->getRepository('AppBundle\Entity\Testresult');
+        $iterator = $testresultRepo->getLatestByTestcaseWithStatisticsIterator($testcase, 1);
+
+        $results = [];
+        foreach ($iterator as $result) {
+            $results[] = $result;
+        }
+
+        $this->assertSame(1, sizeof($results));
+        $this->assertSame('abc', $results[0][0]['id']);
+        $this->assertSame(4, $results[0][1]['numberOf400']);
+    }
+}
