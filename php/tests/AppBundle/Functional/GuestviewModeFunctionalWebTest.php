8d710eb7c59b543b81a4ef99b68abb051d53e212
Manuel Kiessling <manuel@kiessling.net>
2017-02-24 16:51:45 +0100

    Specced feature #36, Guest View mode.

---
+++ php/tests/AppBundle/Functional/GuestviewModeFunctionalWebTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\AppBundle\Functional;
+
+use Tests\AppBundle\TestHelpers;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class GuestviewModeFunctionalWebTest extends WebTestCase
+{
+    use TestHelpers;
+
+    public function testGuestviewMode()
+    {
+        $this->resetDatabase();
+        $client = $this->getClientThatRegisteredAndActivatedADemoUser();
+
+        // This is the testcases page of the demo user, on which the Guest View link must be shown to the logged in demo user
+        $crawler = $client->request('GET', '/testcases/');
+        $link = $crawler->filter('a:contains("Guest View link for this overview")')->eq(0)->link();
+
+        // We need to create a new client because we want to verify that we can visit the guest view page without being logged in
+        $client = static::createClient();
+        $crawler = $client->click($link);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $this->assertNotContains(
+            'Logged in as',
+            $crawler->filter('body')->first()->text()
+        );
+
+        $this->assertEquals('You are viewing this page in Guest View mode. Some functions on this page are not available to you.', trim($crawler->filter('div.alert.alert-warning')->first()->text()));
+        $this->assertEquals('Demo User Testcase One', trim($crawler->filter('h4')->eq(1)->text()));
+        $this->assertEquals('Not available in demo and guest view mode', trim($crawler->filter('div.row div.col-xs-12 a.pull-right')->first()->attr('title')));
+    }
+
+}
